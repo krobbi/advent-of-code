b1ec52256f24025fb126648cb627f293187a5651
krobbi <70552146+krobbi@users.noreply.github.com>
2025-11-16 23:10:24 +0000

    Solve 2015 day 3 part one

--- src/advent_of_code_2015/day_3_perfectly_spherical_houses_in_a_vacuum.rs
+++ src/advent_of_code_2015/day_3_perfectly_spherical_houses_in_a_vacuum.rs
@@ -2,12 +2,40 @@
 //!
 //! [link]: https://adventofcode.com/2015/day/3
 
+use std::collections::HashSet;
+
 use crate::Solution;
 
 /// Solves part one.
 pub fn part_one(input: &str) -> Solution {
-    let _ = input;
-    Solution::default()
+    // Keep track of the set of houses which have been visited.
+    let mut visited_houses = HashSet::new();
+
+    // Also keep track of Santa's position.
+    let (mut x, mut y) = (0i16, 0i16);
+
+    // He has a list of directions.
+    for direction in input.chars() {
+        // And delivers a present before following the direction.
+        visited_houses.insert((x, y));
+
+        let (direction_x, direction_y) = match direction {
+            '^' => (0, 1),
+            'v' => (0, -1),
+            '>' => (1, 0),
+            '<' => (-1, 0),
+            _ => (0, 0),
+        };
+
+        x += direction_x;
+        y += direction_y;
+    }
+
+    // He also visits a house after his last move.
+    visited_houses.insert((x, y));
+
+    // Find the number of houses he visited.
+    visited_houses.len().into()
 }
 
 /// Solves part two.
