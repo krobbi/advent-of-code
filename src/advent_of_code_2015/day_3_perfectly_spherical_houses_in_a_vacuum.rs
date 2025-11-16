1c5d8c177c2b590e610c0203f26aa659ca3fc0c9
krobbi <70552146+krobbi@users.noreply.github.com>
2025-11-16 23:21:19 +0000

    Solve 2015 day 3 part two

--- src/advent_of_code_2015/day_3_perfectly_spherical_houses_in_a_vacuum.rs
+++ src/advent_of_code_2015/day_3_perfectly_spherical_houses_in_a_vacuum.rs
@@ -40,6 +40,47 @@ pub fn part_one(input: &str) -> Solution {
 
 /// Solves part two.
 pub fn part_two(input: &str) -> Solution {
-    let _ = input;
-    Solution::default()
+    // The set of visited houses needs to be kept track of again.
+    let mut visited_houses = HashSet::new();
+
+    // Santa's position is kept track of again.
+    let (mut santa_x, mut santa_y) = (0, 0);
+
+    // He is also joined by Robo-Santa.
+    let (mut robo_x, mut robo_y) = (0, 0);
+
+    // They both deliver a present to the starting house.
+    visited_houses.insert((santa_x, santa_y));
+
+    // This time, Santa and Robo-Santa take turns following the directions.
+    let mut is_santas_turn: bool = true;
+
+    for direction in input.chars() {
+        let (vx, vy) = direction_vector(direction);
+
+        if is_santas_turn {
+            santa_x += vx;
+            santa_y += vy;
+            visited_houses.insert((santa_x, santa_y));
+        } else {
+            robo_x += vx;
+            robo_y += vy;
+            visited_houses.insert((robo_x, robo_y));
+        }
+
+        is_santas_turn = !is_santas_turn;
+    }
+
+    visited_houses.len().into()
+}
+
+/// Returns the vector of a direction.
+fn direction_vector(direction: char) -> (i16, i16) {
+    match direction {
+        '^' => (0, 1),
+        'v' => (0, -1),
+        '>' => (1, 0),
+        '<' => (-1, 0),
+        _ => (0, 0),
+    }
 }
