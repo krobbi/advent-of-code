e4693a9c4bc682b9b5a3585da5497704858e9bc3
krobbi <70552146+krobbi@users.noreply.github.com>
2025-11-17 16:24:19 +0000

    First attempt of 2015 day 6 part one

--- src/advent_of_code_2015/day_6_probably_a_fire_hazard.rs
+++ src/advent_of_code_2015/day_6_probably_a_fire_hazard.rs
@@ -6,15 +6,24 @@ use crate::Solution;
 
 /// Solves part one.
 pub fn part_one(input: &str) -> Solution {
+    // Keep track of non-overlapping rectangles of lights that are turned on.
+    let mut grid = Vec::new();
+
     for instruction in input.lines() {
         let Some((action, rect)) = parse_instruction(instruction) else {
             return Solution::ParseError;
         };
 
-        println!("{action:?} {rect:?}");
+        apply_action(action, rect, &mut grid);
     }
 
-    Solution::default()
+    let mut light_count = 0;
+
+    for rect in grid {
+        light_count += rect.area();
+    }
+
+    light_count.into()
 }
 
 /// Solves part two.
@@ -24,7 +33,7 @@ pub fn part_two(input: &str) -> Solution {
 }
 
 /// An action that can be applied with a [`Rect`].
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Action {
     /// Turn on the lights in the [`Rect`].
     TurnOn,
@@ -36,8 +45,8 @@ enum Action {
     Toggle,
 }
 
-/// An rectangular area of lights.
-#[derive(Clone, Copy, Debug)]
+/// A rectangular area of lights.
+#[derive(Clone, Copy)]
 struct Rect {
     /// The position of the leftmost light.
     left: u16,
@@ -58,6 +67,67 @@ impl Rect {
         let width: u32 = (self.right + 1 - self.left).into();
         let height: u32 = (self.bottom + 1 - self.top).into();
         width * height
+    }
+
+    /// Splits the `Rect` into pieces so it doesn't overlap with another `Rect`.
+    /// This function returns [`None`] if there was no overlap.
+    fn remove_overlap(self, other: Self) -> Option<Box<[Self]>> {        
+        // Do nothing if there is no overlap (AABB algorithm).
+        if other.left > self.right || other.right < self.left || other.top > self.bottom || other.bottom < self.top {
+            return None;
+        }
+
+        let mut pieces = Vec::with_capacity(4);
+
+        // Find the overlapping area.
+        let overlap = Rect {
+            left: self.left.max(other.left),
+            right: self.right.min(other.right),
+            top: self.top.max(other.top),
+            bottom: self.bottom.min(other.bottom),
+        };
+
+        // Add any space to the left of the overlap.
+        if overlap.left > self.left {
+            pieces.push(Rect {
+                left: self.left,
+                right: overlap.left - 1,
+                top: self.top,
+                bottom: self.bottom,
+            });
+        }
+
+        // Add any space to the right of the overlap.
+        if overlap.right < self.right {
+            pieces.push(Rect {
+                left: overlap.right + 1,
+                right: self.right,
+                top: self.top,
+                bottom: self.bottom,
+            });
+        }
+
+        // Add any space above the overlap.
+        if overlap.top > self.top {
+            pieces.push(Rect {
+                left: overlap.left,
+                right: overlap.right,
+                top: self.top,
+                bottom: overlap.top - 1,
+            });
+        }
+
+        // Add any space below the overlap.
+        if overlap.bottom < self.bottom {
+            pieces.push(Rect {
+                left: overlap.left,
+                right: overlap.right,
+                top: overlap.bottom + 1,
+                bottom: self.bottom,
+            });
+        }
+
+        Some(pieces.into_boxed_slice())
     }
 }
 
@@ -90,6 +160,43 @@ fn parse_position(word: &str) -> Option<(u16, u16)> {
     let x = numbers.next()?.parse().ok()?;
     let y = numbers.next()?.parse().ok()?;
     Some((x, y))
+}
+
+/// Applies an [`Action`] with a [`Rect`] to the grid.
+fn apply_action(action: Action, rect: Rect, grid: &mut Vec<Rect>) {
+    let mut new_grid = Vec::new();
+    let mut overlaps = Vec::new();
+
+    // Any overlaps with the new rectangle need to be removed from the grid.
+    while let Some(grid_rect) = grid.pop() {
+        if let Some(pieces) = grid_rect.remove_overlap(rect) {
+            overlaps.push(grid_rect);
+            new_grid.extend_from_slice(&pieces);
+        } else {
+            new_grid.push(grid_rect);
+        }
+    }
+
+    if action == Action::TurnOn {
+        grid.push(rect);
+    } else if action == Action::Toggle {
+        let mut rects = vec![rect];
+        let mut new_rects = Vec::new();
+
+        for overlap in overlaps {
+            while let Some(rect) = rects.pop() {
+                if let Some(pieces) = rect.remove_overlap(overlap) {
+                    new_rects.extend_from_slice(&pieces);
+                } else {
+                    new_rects.push(rect);
+                }
+            }
+
+            rects.append(&mut new_rects);
+        }
+    }
+
+    grid.append(&mut new_grid);
 }
 
 /*
