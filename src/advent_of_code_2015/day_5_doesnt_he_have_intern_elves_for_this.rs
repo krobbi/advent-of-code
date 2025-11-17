ca19ffb0754970d35b196600232bcd16e063db13
krobbi <70552146+krobbi@users.noreply.github.com>
2025-11-17 13:23:30 +0000

    Solve 2015 day 5 part two

--- src/advent_of_code_2015/day_5_doesnt_he_have_intern_elves_for_this.rs
+++ src/advent_of_code_2015/day_5_doesnt_he_have_intern_elves_for_this.rs
@@ -7,17 +7,21 @@ use crate::Solution;
 /// Solves part one.
 pub fn part_one(input: &str) -> Solution {
     // Santa wants to know how many strings in his text file are nice.
-    input.lines().filter(|&l| is_string_nice(l)).count().into()
+    solve_part(input, is_string_nice_part_one)
 }
 
 /// Solves part two.
 pub fn part_two(input: &str) -> Solution {
-    let _ = input;
-    Solution::default()
+    solve_part(input, is_string_nice_part_two)
 }
 
-/// Returns `true` if a string is nice.
-fn is_string_nice(string: &str) -> bool {
+/// Solves a part with a predicate for whether a string is nice.
+fn solve_part(input: &str, is_string_nice: fn(string: &str) -> bool) -> Solution {
+    input.lines().filter(|&l| is_string_nice(l)).count().into()
+}
+
+/// Returns `true` if a string is nice for part one.
+fn is_string_nice_part_one(string: &str) -> bool {
     // Nice strings contain at least three vowels.
     let mut vowel_count = 0;
 
@@ -41,4 +45,38 @@ fn is_string_nice(string: &str) -> bool {
     }
 
     has_double && vowel_count >= 3
+}
+
+/// Returns `true` if a string is nice for part two.
+fn is_string_nice_part_two(string: &str) -> bool {
+    // Convert the string to fixed-size characters. This allows sliding windows
+    // and faster indexing.
+    let string: Box<[char]> = string.chars().collect();
+
+    // A string is nice if any pair of letters appears at least twice. Overlaps
+    // of the same three letters don't count.
+    let mut has_matching_pairs = false;
+
+    'check_pairs: for (index, pair) in string.windows(2).map(|w| (w[0], w[1])).enumerate() {
+        for other_pair in string[index + 2..].windows(2).map(|w| (w[0], w[1])) {
+            if pair == other_pair {
+                has_matching_pairs = true;
+                break 'check_pairs;
+            }
+        }
+    }
+
+    if !has_matching_pairs {
+        return false;
+    }
+
+    // Nice strings must also have a repeated letter with any letter between
+    // them.
+    for window in string.windows(3) {
+        if window[0] == window[2] {
+            return true;
+        }
+    }
+
+    false
 }
