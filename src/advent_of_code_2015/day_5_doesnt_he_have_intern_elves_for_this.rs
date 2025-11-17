9558a5535909038cee5761769b8565736d35a420
krobbi <70552146+krobbi@users.noreply.github.com>
2025-11-17 12:27:51 +0000

    Solve 2015 day 5 part one

--- src/advent_of_code_2015/day_5_doesnt_he_have_intern_elves_for_this.rs
+++ src/advent_of_code_2015/day_5_doesnt_he_have_intern_elves_for_this.rs
@@ -6,12 +6,39 @@ use crate::Solution;
 
 /// Solves part one.
 pub fn part_one(input: &str) -> Solution {
-    let _ = input;
-    Solution::default()
+    // Santa wants to know how many strings in his text file are nice.
+    input.lines().filter(|&l| is_string_nice(l)).count().into()
 }
 
 /// Solves part two.
 pub fn part_two(input: &str) -> Solution {
     let _ = input;
     Solution::default()
+}
+
+/// Returns `true` if a string is nice.
+fn is_string_nice(string: &str) -> bool {
+    // Nice strings contain at least three vowels.
+    let mut vowel_count = 0;
+
+    // Nice strings contain at least one letter that appears twice in a row.
+    let mut previous_letter = '\0';
+    let mut has_double = false;
+
+    // Nice strings must not contain the pairs "ab", "cd", "pq", or "xy".
+    for letter in string.chars() {
+        match (previous_letter, letter) {
+            (_, 'a' | 'e' | 'i' | 'o'| 'u') => vowel_count += 1,
+            ('a', 'b') | ('c', 'd') | ('p', 'q') | ('x', 'y') => return false,
+            _ => (),
+        }
+
+        if letter == previous_letter {
+            has_double = true;
+        }
+
+        previous_letter = letter;
+    }
+
+    has_double && vowel_count >= 3
 }
