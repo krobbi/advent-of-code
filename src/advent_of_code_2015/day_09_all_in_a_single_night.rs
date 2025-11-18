e5b8107113e24f6f428daa8afa6549b6d7fde5d3
krobbi <70552146+krobbi@users.noreply.github.com>
2025-11-18 21:29:56 +0000

    Optimize 2015 day 9

--- src/advent_of_code_2015/day_09_all_in_a_single_night.rs
+++ src/advent_of_code_2015/day_09_all_in_a_single_night.rs
@@ -2,178 +2,161 @@
 //!
 //! [link]: https://adventofcode.com/2015/day/9
 
-use std::collections::HashMap;
-
 use crate::Solution;
 
 /// Solves part one.
 pub fn part_one(input: &str) -> Solution {
     // Santa needs to find the shortest distance to visit each location in a set
     // of locations (travelling salesman problem).
-    let Some(mut map) = parse_map(input) else {
-        return Solution::ParseError;
-    };
-
-    let mut min_distance = usize::MAX;
-
-    for name in map.location_names() {
-        find_best_distance(&mut map, &name, 0, &mut min_distance, false);
+    if let Some((shortest_distance, _)) = solve_parts(input) {
+        shortest_distance.into()
+    } else {
+        Solution::ParseError
     }
-
-    min_distance.into()
 }
 
 /// Solves part two.
 pub fn part_two(input: &str) -> Solution {
     // Now Santa wants to show off and take the longest distance. This is
     // basically the same problem.
-    let Some(mut map) = parse_map(input) else {
-        return Solution::ParseError;
-    };
-
-    let mut max_distance = 0;
-
-    for name in map.location_names() {
-        find_best_distance(&mut map, &name, 0, &mut max_distance, true);
+    if let Some((_, longest_distance)) = solve_parts(input) {
+        longest_distance.into()
+    } else {
+        Solution::ParseError
     }
-
-    max_distance.into()
 }
 
-/// Recursively visits every [`Location`] in a map and updates a best minimum or
-/// maximum distance to cover a [`Map`].
-fn find_best_distance(
-    map: &mut Map,
-    name: &str,
-    distance: usize,
-    best_distance: &mut usize,
-    is_max: bool,
-) {
-    map.visit(name);
-
-    // If the entire map has been visited, then a new best distance may have
-    // been found.
-    if map.is_all_visited() {
-        *best_distance = if is_max {
-            (*best_distance).max(distance)
-        } else {
-            (*best_distance).min(distance)
-        };
-    }
-
-    for (name, link_distance) in map.links(name) {
-        // Don't revisit locations.
-        if map.is_visited(&name) {
-            continue;
-        }
-
-        find_best_distance(map, &name, distance + link_distance, best_distance, is_max);
-    }
-
-    map.unvist(name);
+/// Solves a part and returns the shortest and longest distances. This function
+/// returns [`None`] if the list of locations could not be parsed.
+fn solve_parts(input: &str) -> Option<(u16, u16)> {
+    let graph = parse_graph(input)?;
+    let mut visitor = Visitor::new();
+    visitor.visit_graph(&graph);
+    Some((visitor.shortest_distance, visitor.longest_distance))
 }
 
-/// A map of [`Location`]s.
-#[derive(Default)]
-struct Map {
-    /// The map of names to [`Location`]s.
-    locations: HashMap<String, Location>,
+/// A structure which visits a [`Graph`] and finds the shortest and longest
+/// distances through its nodes.
+struct Visitor {
+    /// The stack of currently visited nodes.
+    visited_nodes: Vec<usize>,
+
+    /// The shortest distance through the `Graph`.
+    shortest_distance: u16,
+
+    /// The longest distance through the `Graph`.
+    longest_distance: u16,
 }
 
-impl Map {
-    /// Creates a new `Map`.
+impl Visitor {
+    /// Creates a new `Visitor`.
     fn new() -> Self {
-        Self::default()
+        Self {
+            visited_nodes: Vec::new(),
+            shortest_distance: u16::MAX,
+            longest_distance: 0,
+        }
     }
 
-    /// Returns the names of every [`Location`].
-    fn location_names(&self) -> Box<[String]> {
-        self.locations.keys().cloned().collect()
+    /// Visits a [`Graph`].
+    fn visit_graph(&mut self, graph: &Graph) {
+        for node in 0..graph.node_count {
+            self.visit_node(graph, node, 0);
+        }
     }
 
-    /// Returns the links from a [`Location`] name.
-    fn links(&self, name: &str) -> HashMap<String, usize> {
-        self.location(name).links.clone()
-    }
+    /// Visits a node in a [`Graph`] with a total distance.
+    fn visit_node(&mut self, graph: &Graph, node: usize, distance: u16) {
+        self.visited_nodes.push(node);
 
-    /// Visits a [`Location`] from its name.
-    fn visit(&mut self, name: &str) {
-        self.location_mut(name).is_visited = true;
-    }
+        if self.visited_nodes.len() == graph.node_count {
+            self.shortest_distance = self.shortest_distance.min(distance);
+            self.longest_distance = self.longest_distance.max(distance);
+        } else {
+            for (next_node, link_distance) in graph.distances_from(node).iter().copied().enumerate()
+            {
+                if next_node == node || self.visited_nodes.contains(&next_node) {
+                    continue;
+                }
 
-    /// Stops visiting a [`Location`] from its name.
-    fn unvist(&mut self, name: &str) {
-        self.location_mut(name).is_visited = false;
-    }
-
-    /// Returns `true` if a [`Location`] is visited from its name.
-    fn is_visited(&self, name: &str) -> bool {
-        self.location(name).is_visited
-    }
-
-    /// Returns `true` if every [`Location`] is visited.
-    fn is_all_visited(&self) -> bool {
-        for location in self.locations.values() {
-            if !location.is_visited {
-                return false;
+                self.visit_node(graph, next_node, distance + link_distance);
             }
         }
 
-        true
-    }
-
-    /// Adds a two-way link between two [`Location`]s from their names.
-    fn add_link(&mut self, name_a: &str, name_b: &str, distance: usize) {
-        let location_a = self.location_mut(name_a);
-        location_a.add_link(name_b, distance);
-        let location_b = self.location_mut(name_b);
-        location_b.add_link(name_a, distance);
-    }
-
-    /// Returns a reference to a [`Location`] from its name.
-    fn location(&self, name: &str) -> &Location {
-        self.locations.get(name).expect("location should exist")
-    }
-
-    /// Returns a mutable reference to a [`Location`] from its name.
-    fn location_mut(&mut self, name: &str) -> &mut Location {
-        self.locations.entry(name.to_owned()).or_default()
+        self.visited_nodes.pop();
     }
 }
 
-/// A location.
+/// A graph of distances between locations.
+struct Graph {
+    /// The number of nodes in the `Graph`.
+    node_count: usize,
+
+    /// The matrix of distances between nodes.
+    links: Vec<u16>,
+}
+
+impl Graph {
+    /// Creates a new `Graph` with a node count.
+    fn new(node_count: usize) -> Self {
+        Self {
+            node_count,
+            links: vec![0; node_count * node_count],
+        }
+    }
+
+    /// Returns a slice of distances from a node.
+    fn distances_from(&self, node: usize) -> &[u16] {
+        let index = node * self.node_count;
+        &self.links[index..index + self.node_count]
+    }
+}
+
+/// A map of location names to nodes.
 #[derive(Default)]
-struct Location {
-    /// Whether the `Location` is currently visited.
-    is_visited: bool,
-
-    /// The distances to every linked `Location`.
-    links: HashMap<String, usize>,
+struct LocationTable {
+    /// The location names.
+    location_names: Vec<String>,
 }
 
-impl Location {
-    /// Adds a link from the `Location`.
-    fn add_link(&mut self, name: &str, distance: usize) {
-        self.links.insert(name.to_owned(), distance);
+impl LocationTable {
+    /// Returns a node from a location name.
+    fn node(&mut self, name: &str) -> usize {
+        if let Some(node) = self.location_names.iter().position(|n| n == name) {
+            node
+        } else {
+            let node = self.location_names.len();
+            self.location_names.push(name.to_owned());
+            node
+        }
     }
 }
 
-/// Parses a [`Map`] from a list of distances. This function returns [`None`] if
-/// a [`Map`] could not be parsed.
-fn parse_map(list: &str) -> Option<Map> {
-    let mut map = Map::new();
+/// Parses a [`Graph`] from a list of distances. This function returns [`None`]
+/// if a [`Graph`] could not be parsed.
+fn parse_graph(input: &str) -> Option<Graph> {
+    let mut location_table = LocationTable::default();
+    let mut links = Vec::new();
 
-    for item in list.lines() {
-        let mut words = item.split(' ');
-        let name_a = words.next()?;
+    for line in input.lines() {
+        let mut words = line.split(' ');
+        let node_a = location_table.node(words.next()?);
         words.next()?; // Skip "to".
-        let name_b = words.next()?;
+        let node_b = location_table.node(words.next()?);
         words.next()?; // Skip "=".
         let distance = words.next()?.parse().ok()?;
-        map.add_link(name_a, name_b, distance);
+        links.push((node_a, node_b, distance));
     }
 
-    Some(map)
+    let node_count = location_table.location_names.len();
+    let mut graph = Graph::new(node_count);
+
+    for (node_a, node_b, distance) in links {
+        graph.links[node_a * node_count + node_b] = distance;
+        graph.links[node_b * node_count + node_a] = distance;
+    }
+
+    Some(graph)
 }
 
 /*
