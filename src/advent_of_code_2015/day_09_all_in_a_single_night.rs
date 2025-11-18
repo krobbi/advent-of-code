7d7cf46f5243e90bec30ec2a4dc906f755e05fbc
krobbi <70552146+krobbi@users.noreply.github.com>
2025-11-18 19:14:14 +0000

    Solve 2015 day 9 part one

--- src/advent_of_code_2015/day_09_all_in_a_single_night.rs
+++ src/advent_of_code_2015/day_09_all_in_a_single_night.rs
@@ -10,12 +10,17 @@ use crate::Solution;
 pub fn part_one(input: &str) -> Solution {
     // Santa needs to find the shortest distance to visit each location in a set
     // of locations (travelling salesman problem).
-    let Some(map) = parse_map(input) else {
+    let Some(mut map) = parse_map(input) else {
         return Solution::ParseError;
     };
 
-    println!("{map:#?}");
-    Solution::default()
+    let mut min_distance = usize::MAX;
+
+    for name in map.location_names() {
+        find_min_distance(&mut map, &name, 0, &mut min_distance);
+    }
+
+    min_distance.into()
 }
 
 /// Solves part two.
@@ -24,8 +29,31 @@ pub fn part_two(input: &str) -> Solution {
     Solution::default()
 }
 
+/// Recursively visits every [`Location`] in a map and updates a minimum
+/// distance to cover the [`Map`].
+fn find_min_distance(map: &mut Map, name: &str, distance: usize, min_distance: &mut usize) {
+    map.visit(name);
+
+    // If the entire map has been visited, then a new shortest distance may have
+    // been found.
+    if map.is_all_visited() {
+        *min_distance = (*min_distance).min(distance);
+    }
+
+    for (name, link_distance) in map.links(name) {
+        // Don't revisit locations.
+        if map.is_visited(&name) {
+            continue;
+        }
+
+        find_min_distance(map, &name, distance + link_distance, min_distance);
+    }
+
+    map.unvist(name);
+}
+
 /// A map of [`Location`]s.
-#[derive(Debug, Default)]
+#[derive(Default)]
 struct Map {
     /// The map of names to [`Location`]s.
     locations: HashMap<String, Location>,
@@ -37,12 +65,53 @@ impl Map {
         Self::default()
     }
 
-    /// Adds a two-way link between two [`Location`]s.
+    /// Returns the names of every [`Location`].
+    fn location_names(&self) -> Box<[String]> {
+        self.locations.keys().cloned().collect()
+    }
+
+    /// Returns the links from a [`Location`] name.
+    fn links(&self, name: &str) -> HashMap<String, usize> {
+        self.location(name).links.clone()
+    }
+
+    /// Visits a [`Location`] from its name.
+    fn visit(&mut self, name: &str) {
+        self.location_mut(name).is_visited = true;
+    }
+
+    /// Stops visiting a [`Location`] from its name.
+    fn unvist(&mut self, name: &str) {
+        self.location_mut(name).is_visited = false;
+    }
+
+    /// Returns `true` if a [`Location`] is visited from its name.
+    fn is_visited(&self, name: &str) -> bool {
+        self.location(name).is_visited
+    }
+
+    /// Returns `true` if every [`Location`] is visited.
+    fn is_all_visited(&self) -> bool {
+        for location in self.locations.values() {
+            if !location.is_visited {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    /// Adds a two-way link between two [`Location`]s from their names.
     fn add_link(&mut self, name_a: &str, name_b: &str, distance: usize) {
         let location_a = self.location_mut(name_a);
         location_a.add_link(name_b, distance);
         let location_b = self.location_mut(name_b);
         location_b.add_link(name_a, distance);
+    }
+
+    /// Returns a reference to a [`Location`] from its name.
+    fn location(&self, name: &str) -> &Location {
+        self.locations.get(name).expect("location should exist")
     }
 
     /// Returns a mutable reference to a [`Location`] from its name.
@@ -52,10 +121,10 @@ impl Map {
 }
 
 /// A location.
-#[derive(Debug, Default)]
+#[derive(Default)]
 struct Location {
     /// Whether the `Location` is currently visited.
-    visited: bool,
+    is_visited: bool,
 
     /// The distances to every linked `Location`.
     links: HashMap<String, usize>,
