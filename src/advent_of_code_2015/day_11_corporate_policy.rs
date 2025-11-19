3b9da8476990749cc27767f6dcbf2db2894b790f
krobbi <70552146+krobbi@users.noreply.github.com>
2025-11-19 11:11:14 +0000

    Parse password for 2015 day 11

--- src/advent_of_code_2015/day_11_corporate_policy.rs
+++ src/advent_of_code_2015/day_11_corporate_policy.rs
@@ -2,11 +2,18 @@
 //!
 //! [link]: https://adventofcode.com/2015/day/11
 
+use std::fmt::{self, Display, Formatter, Write as _};
+
 use crate::Solution;
 
 /// Solves part one.
 pub fn part_one(input: &str) -> Solution {
-    let _ = input;
+    let Some(password) = parse_password(input) else {
+        return Solution::ParseError;
+    };
+
+    println!("{password:?}");
+    println!("{password}");
     Solution::default()
 }
 
@@ -14,6 +21,49 @@ pub fn part_one(input: &str) -> Solution {
 pub fn part_two(input: &str) -> Solution {
     let _ = input;
     Solution::default()
+}
+
+/// An eight-letter password.
+#[derive(Clone, Copy, Debug)]
+struct Password {
+    /// The letters of the `Password` where "a" is 0 and "z" is 25.
+    letters: [u8; 8],
+}
+
+impl Display for Password {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut string = String::with_capacity(8);
+
+        for letter in self.letters {
+            let _ = string.write_char((letter + b'a').into());
+        }
+
+        f.write_str(&string)
+    }
+}
+
+/// Parses a [`Password`] from a string. This function returns [`None`] if a
+/// [`Password`] could not be parsed.
+fn parse_password(input: &str) -> Option<Password> {
+    let input = input.as_bytes();
+
+    if input.len() < 8 {
+        return None;
+    }
+
+    let mut letters = [0; 8];
+
+    for index in 0..8 {
+        let letter = input[index];
+
+        if !letter.is_ascii_lowercase() {
+            return None;
+        }
+
+        letters[index] = letter - b'a';
+    }
+
+    Some(Password { letters })
 }
 
 /*
