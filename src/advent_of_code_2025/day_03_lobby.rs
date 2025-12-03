622bd4ed52e8ea3ae1eade5bfbaf3627519ad244
krobbi <70552146+krobbi@users.noreply.github.com>
2025-12-03 08:17:26 +0000

    Add 2025 day 3

---
+++ src/advent_of_code_2025/day_03_lobby.rs
@@ -0,0 +1,38 @@
+//! [Day 3: Lobby][link]
+//!
+//! [link]: https://adventofcode.com/2025/day/3
+
+use crate::Solution;
+
+/// Solves part one.
+pub fn part_one(input: &str) -> Solution {
+    // The elevators to the rest of the North Pole Base are offline. They need
+    // emergency power from battery banks (lines of digits) where each battery
+    // has a joltage rating (single digit). Two batteries from each bank can be
+    // turned on and the total joltage from the bank is the number as it is read
+    // out. The highest joltage from "12345" would be "45", not "54". We need to
+    // find the sum of the highest joltages from each bank.
+    let _ = input;
+    Solution::default()
+}
+
+/// Solves part two.
+pub fn part_two(input: &str) -> Solution {
+    let _ = input;
+    Solution::default()
+}
+
+/*
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Tests part one.
+    #[test]
+    fn part_one_works() {}
+
+    /// Tests part two.
+    #[test]
+    fn part_two_works() {}
+}
+*/
