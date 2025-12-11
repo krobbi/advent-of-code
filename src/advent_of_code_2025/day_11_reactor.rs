0007d526c5927c21b75a03dc2c0d52a592ad2679
krobbi <70552146+krobbi@users.noreply.github.com>
2025-12-11 09:08:05 +0000

    Parse input for 2025 day 11

--- src/advent_of_code_2025/day_11_reactor.rs
+++ src/advent_of_code_2025/day_11_reactor.rs
@@ -2,6 +2,8 @@
 //!
 //! [link]: https://adventofcode.com/2025/day/11
 
+use std::collections::HashMap;
+
 use crate::Solution;
 
 /// Solves part one.
@@ -16,7 +18,10 @@ pub fn part_one(input: &str) -> Solution {
     // output.
 
     // This sounds like traversing a directed (hopefully acyclic) graph.
-    let _ = input;
+    let Some(_network) = parse_network(input) else {
+        return Solution::ParseError;
+    };
+
     Solution::default()
 }
 
@@ -24,6 +29,52 @@ pub fn part_one(input: &str) -> Solution {
 pub fn part_two(input: &str) -> Solution {
     let _ = input;
     Solution::default()
+}
+
+/// A data network.
+#[derive(Default)]
+struct Network {
+    /// The map of device names to `Device`s.
+    devices: HashMap<String, Device>,
+}
+
+/// A device in a [`Network`].
+#[derive(Default)]
+struct Device {
+    /// The connections to other device names from the `Device`.
+    connections: Vec<String>,
+}
+
+/// Parses a [`Network`] from input. This function returns [`None`] if a
+/// [`Network`] could not be parsed.
+fn parse_network(input: &str) -> Option<Network> {
+    let mut network = Network::default();
+
+    for line in input.lines().map(str::trim) {
+        let (device_name, device) = parse_device(line)?;
+        network.devices.insert(device_name, device);
+    }
+
+    Some(network)
+}
+
+/// Parses a device name and [`Device`] from a line of input. This function
+/// returns [`None`] if a [`Device`] could not be parsed.
+fn parse_device(line: &str) -> Option<(String, Device)> {
+    let mut device_names = line.split_whitespace();
+    let device_name = device_names.next()?.trim_end_matches(':');
+
+    if device_name.is_empty() {
+        return None;
+    }
+
+    let mut device = Device::default();
+
+    for connected_device_name in device_names {
+        device.connections.push(connected_device_name.into());
+    }
+
+    Some((device_name.into(), device))
 }
 
 /*
